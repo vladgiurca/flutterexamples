420ae34405470d81898de3137dddc8eb7bb20ced
Vlad Giurca <vladgiurca@gmail.com>
2020-11-13 19:46:36 +0200

    create HomePage Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          title: Text('FlutterEx'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Testing'),
-              RaisedButton(
-                  onPressed: () {},
-                  child: Text('Press Me'),
-                  color: Colors.amber),
-            ],
-          ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlutterEx'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Testing'),
+            RaisedButton(
+                onPressed: () {}, child: Text('Press Me'), color: Colors.amber),
+          ],
         ),
       ),
     );
